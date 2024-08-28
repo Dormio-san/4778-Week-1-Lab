badcfc93f9f41dab965f2d788facc30e501b0e96
Dormio-san <liam.lovett06@gmail.com>
2024-08-28 18:03:47 -0400

    Begin work on the chess board spawning script.

--- ChessLab/Assets/Scripts/SpawnChessBoard.cs
+++ ChessLab/Assets/Scripts/SpawnChessBoard.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class SpawnChessBoard : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnDrawGizmos()
     {
+        for (int i = 0; i < 8; i ++)
+        {
+            for (int j = 0; j < 8; j++)
+            {
+                Gizmos.color = new Color(0, 0, 0);
+                Gizmos.DrawWireCube(new Vector3(j + (i * 2), j, j), new Vector3(1, 1, 1));
+            }
+        }
         
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
+        //for (int i = 0; i < 4; i ++)
+        //{
+        //    int loopsWhiteSquares = 7;
+        //    loopsWhiteSquares = loopsWhiteSquares - 2;
+
+        //    for (int j = 0; j < loopsWhiteSquares; j++)
+        //    {
+        //        Gizmos.color = new Color(1, 1, 1);
+        //        Gizmos.DrawWireCube(new Vector3(j + 1, j, j), new Vector3(1, 1, 1));
+        //    }
+        //}
         
     }
 }
